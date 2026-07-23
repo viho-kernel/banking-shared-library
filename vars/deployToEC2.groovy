3c9d6013039d6f1f1f84cfaf9a7ae7bb4b227ade
viho-kernel <vihari.reddy1802@gmail.com>
2026-07-23 13:01:31 +0530

    latest commit

---
+++ vars/deployToEC2.groovy
@@ -0,0 +1,24 @@
+def call(String host, String image, String tag) {
+    sh """
+
+    ssh -o StrictHostKeyChecking=no ec2-user@${host} << EOF
+
+    aws ecr get-login-password --region us-east-1 | docker login \
+    --username AWS \
+    --password-stdin 992989046853.dkr.ecr.us-east-1.amazonaws.com
+
+    docker pull ${image}:${tag}
+
+    docker stop abc-banking || true
+
+    docker rm abc-banking || true
+
+    docker run -d \
+      --name abc-banking \
+      --restart unless-stopped \
+      -p 3000:3000 \
+      ${image}:${tag}
+
+    EOF
+    """
+}
