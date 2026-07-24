a148e42ad733f75d69a536356843658f307a96e3
viho-kernel <vihari.reddy1802@gmail.com>
2026-07-24 11:44:43 +0530

    next commit

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -1,31 +1,27 @@
 def call(String host, String image, String tag) {
 
-sh """
+    sh """
 ssh -o StrictHostKeyChecking=no ec2-user@${host} "
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 992989046853.dkr.ecr.us-east-1.amazonaws.com && \
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 992989046853.dkr.ecr.us-east-1.amazonaws.com && \\
+docker pull ${image}:${tag} && \\
+docker stop abc-banking || true && \\
+docker rm abc-banking || true && \\
+docker run -d --name abc-banking --restart unless-stopped -p 3000:3000 ${image}:${tag} && \\
 
-docker pull ${image}:${tag} && \
-
-docker stop abc-banking || true && \
-docker rm abc-banking || true && \
-
-docker run -d --name abc-banking --restart unless-stopped -p 3000:3000 ${image}:${tag} && \
-
-echo 'Waiting for application to start...' && \
+echo 'Waiting for application to start...'
 
 for i in {1..5}
+do
+    if curl -fs http://localhost:3000 > /dev/null
+    then
+        echo 'Application is healthy.'
+        exit 0
+    fi
 
-#usually -f fails on HTTP error so we use -s silent mode.
-
-do 
-  if curl -fs http://localhost:3000 > /dev/null
-  then
-     echo 'Application is healthy.'
-     exit 0
-  fi
-  echo "Attempt $i failed. Retrying in 5 seconds.."
-  sleep 5
+    echo "Attempt \$i failed. Retrying in 5 seconds..."
+    sleep 5
 done
+
 echo 'Application failed to become healthy.'
 exit 1
 "
