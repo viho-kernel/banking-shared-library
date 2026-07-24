1af4a103bfe78e756c6f93b44c68932ae2e8870a
viho-kernel <vihari.reddy1802@gmail.com>
2026-07-24 10:17:15 +0530

    latst commit

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -1,11 +1,21 @@
 def call(String host, String image, String tag) {
 
     sh """
-ssh -o StrictHostKeyChecking=no ec2-user@${host} \
-"aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 992989046853.dkr.ecr.us-east-1.amazonaws.com && \
+ssh -o StrictHostKeyChecking=no ec2-user@${host} "
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 992989046853.dkr.ecr.us-east-1.amazonaws.com && \
+
 docker pull ${image}:${tag} && \
+
 docker stop abc-banking || true && \
 docker rm abc-banking || true && \
-docker run -d --name abc-banking --restart unless-stopped -p 3000:3000 ${image}:${tag}"
+
+docker run -d --name abc-banking --restart unless-stopped -p 3000:3000 ${image}:${tag} && \
+
+echo 'Waiting for application to start...' && \
+sleep 15 && \
+
+curl -f http://localhost:3000 > /dev/null && \
+echo 'Application is healthy.'
+"
 """
 }
