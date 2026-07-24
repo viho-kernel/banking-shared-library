7595c3c44ff643ef41ccccadf701df1953cf71e1
viho-kernel <vihari.reddy1802@gmail.com>
2026-07-24 11:30:16 +0530

    made changes in attempting

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -12,10 +12,25 @@ docker rm abc-banking || true && \
 docker run -d --name abc-banking --restart unless-stopped -p 3000:3000 ${image}:${tag} && \
 
 echo 'Waiting for application to start...' && \
-sleep 15 && \
 
-curl -f http://localhost:3000 > /dev/null && \
-echo 'Application is healthy.'
+for i in {1..12}
+
+#usually -f fails on HTTP error so we use -s silent mode.
+
+do 
+  if curl -fs http://localhost:3000 > /dev/null
+  then
+     echo 'Application is healthy.'
+     exit 0
+  fi
+
+  echo "Attempt $i failed. Retrying in 5 seconds.."
+  sleep 5
+done
+
+echo 'Application failed to become healthy.'
+
+exit 1
 "
 """
 }
