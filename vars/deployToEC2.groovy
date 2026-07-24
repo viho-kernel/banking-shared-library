72b9bec9b01fff8a5430f7d17d2f4fa4ad042491
viho-kernel <vihari.reddy1802@gmail.com>
2026-07-24 12:17:53 +0530

    adding a line to get current image

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -4,6 +4,8 @@ def call(String host, String image, String tag) {
 ssh -o StrictHostKeyChecking=no ec2-user@${host} "
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 992989046853.dkr.ecr.us-east-1.amazonaws.com && \\
 docker pull ${image}:${tag} && \\
+CURRENT_IMAGE=\$(docker inspect abc-banking --format='{{.Config.Image}}' 2>/dev/null || true)
+echo "Current running image: \$CURRENT_IMAGE"
 docker stop abc-banking || true && \\
 docker rm abc-banking || true && \\
 docker run -d --name abc-banking --restart unless-stopped -p 3000:3000 ${image}:${tag} && \\
